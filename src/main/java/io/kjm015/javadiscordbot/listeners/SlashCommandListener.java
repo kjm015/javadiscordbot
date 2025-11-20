133e8efd0f73c99eaf5a2b2e2ab794ae3436f07b
kjm015 <kjm015@yahoo.com>
2025-11-20 17:58:47 -0600

    add logger to command listener

--- src/main/java/io/kjm015/javadiscordbot/listeners/SlashCommandListener.java
+++ src/main/java/io/kjm015/javadiscordbot/listeners/SlashCommandListener.java
@@ -3,15 +3,21 @@ package io.kjm015.javadiscordbot.listeners;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public class SlashCommandListener extends ListenerAdapter {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
 
         if (event.getName().equals("say")) {
             String content = event.getOption("content", OptionMapping::getAsString);
+            log.info("Received command: /say {}", content);
             event.reply(Objects.requireNonNullElse(content, "You need to provide a message to say!")).queue();
         }
 
