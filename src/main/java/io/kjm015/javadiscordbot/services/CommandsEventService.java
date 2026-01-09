1d3eba5af5ad66b8392ba8389143d6a3494320e5
kjm015 <kjm015@yahoo.com>
2026-01-09 17:25:44 -0600

    add logger to commands event service

--- src/main/java/io/kjm015/javadiscordbot/services/CommandsEventService.java
+++ src/main/java/io/kjm015/javadiscordbot/services/CommandsEventService.java
@@ -4,12 +4,16 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 import io.kjm015.javadiscordbot.conifg.CommandsSettings;
 import io.kjm015.javadiscordbot.models.CommandEventRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
 public class CommandsEventService {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final RestClient client;
     private final CommandsSettings settings;
@@ -21,6 +25,8 @@ public class CommandsEventService {
     }
 
     public void saveCommandEvent(String name, String content) {
+        log.info("Sending request to save command event: {} {}", name, content);
+
         var request = new CommandEventRequest(name, content);
         client.post()
                 .uri(settings.getUrl() + "/save")
